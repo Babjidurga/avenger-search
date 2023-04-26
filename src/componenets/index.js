4c647a5969ab9f14ffb0a567a275cb31e1b4a612
Babjidurga <Babji2662@gmail.com>
2023-04-26 21:22:22 +0530

    This is the code from VS code

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -2,21 +2,39 @@ import { Component } from "react";
 import './index.css'
 
 class Counter extends Component{
-    state = {text:true,content:"LightMode"}
-    changeText = () => {
-        const {text,content} =  this.state
+    state = {text:true,firstOne:"hide",lastOne:"hide"}
+    first = () => {
+        const {text,firstOne} =  this.state
         if (text){
-            this.setState({text:false,content:"DarkMode"})
+            this.setState({text:false,firstOne:"show"})
         }else{
-            this.setState({text:true,content:"LightMode"})
+            this.setState({text:true,firstOne:"hide"})
+        }
+    }
+    last = () => {
+        const {text,lastOne} =  this.state
+        if (text){
+            this.setState({text:false,lastOne:"hide"})
+        }else{
+            this.setState({text:true,lastOne:"show"})
         }
     }
     render(){
-        const {content} =  this.state
+        const {firstOne,lastOne} =  this.state
         return(
-            <div className= {`container ${content}`} >
-                    <h1>Click to change the mode</h1>
-                    <button onClick={this.changeText}> {content}</button>
+        
+            <div className= "container" >
+                <h2>SHOW / HIDE</h2>
+                <div className="data">
+                    <div className="name-container">
+                        <button onClick={this.first}> Show / Hide First name</button>
+                        <h1 className={` ${firstOne}`}>John</h1>
+                    </div>
+                    <div className="name-container">
+                        <button onClick={this.last}> Show / Hide Last name</button>
+                        <h1 className={` ${lastOne}`}>Snow</h1>
+                    </div>
+                    </div>
             </div>
         )
     }
