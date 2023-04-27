532711b53543a1a70197720562e10ac7cbe2f270
Babjidurga <Babji2662@gmail.com>
2023-04-27 10:39:27 +0530

    This is the simple code

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -2,26 +2,20 @@ import { Component } from "react";
 import './index.css'
 
 class Counter extends Component{
-    state = {count:0}
-
-    onIncrease = () => {
-        const number = Math.ceil(Math.random() * 100)
-        this.setState((prevState) => ({count: prevState.count + number}))
+    state = {status:false}
+    changeData = () => {
+        this.setState((prevState) => ({status: !prevState.status}))
     }
-    
-    
     render(){
-        const {count} =  this.state
-        const text = count % 2 === 0 ? "Even":"Odd";
+        const {status} = this.state
+        const headText = status ? "Welcome User":"Please Login"
+        const buttonText = status ? "Logout":"Login"
         return(
         
-            <div className= "container" >
-                <h1>Count {count}</h1>
-                <h3>Count is {text}</h3>
-                <button onClick = {this.onIncrease}>Increment</button>
-                <p>* Increase by random number between 0 and 100</p>
-                
-            </div>
+           <div className="container">
+                <h1>{headText}</h1>
+                <button onClick={this.changeData}>{buttonText}</button>
+           </div>
         )
     }
 }
