764eeccc5a38862b4435bd120ad7ce6b55f46aa7
Babjidurga <Babji2662@gmail.com>
2023-04-27 10:17:11 +0530

    This is the code from Vs code

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -2,39 +2,25 @@ import { Component } from "react";
 import './index.css'
 
 class Counter extends Component{
-    state = {text:true,firstOne:"hide",lastOne:"hide"}
-    first = () => {
-        const {text,firstOne} =  this.state
-        if (text){
-            this.setState({text:false,firstOne:"show"})
-        }else{
-            this.setState({text:true,firstOne:"hide"})
-        }
+    state = {count:0}
+
+    onIncrease = () => {
+        const number = Math.ceil(Math.random() * 100)
+        this.setState((prevState) => ({count: prevState.count + number}))
     }
-    last = () => {
-        const {text,lastOne} =  this.state
-        if (text){
-            this.setState({text:false,lastOne:"hide"})
-        }else{
-            this.setState({text:true,lastOne:"show"})
-        }
-    }
+    
+    
     render(){
-        const {firstOne,lastOne} =  this.state
+        const {count} =  this.state
+        const text = count % 2 === 0 ? "Even":"Odd";
         return(
         
             <div className= "container" >
-                <h2>SHOW / HIDE</h2>
-                <div className="data">
-                    <div className="name-container">
-                        <button onClick={this.first}> Show / Hide First name</button>
-                        <h1 className={` ${firstOne}`}>John</h1>
-                    </div>
-                    <div className="name-container">
-                        <button onClick={this.last}> Show / Hide Last name</button>
-                        <h1 className={` ${lastOne}`}>Snow</h1>
-                    </div>
-                    </div>
+                <h1>Count {count}</h1>
+                <h3>Count is {text}</h3>
+                <button onClick = {this.onIncrease}>Increment</button>
+                <p>* Increase by random number between 0 and 100</p>
+                
             </div>
         )
     }
